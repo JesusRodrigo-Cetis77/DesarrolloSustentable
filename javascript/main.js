0f53a6e5a727435634ce240610cc2798079b77b2
JesusRodrigo-cetis77 <jesus.rodrigo.estrada.cetis77@gmail.com>
2021-11-29 13:54:54 -0600

    Eliminación y organizacion de archivos para evitar tener de mas

---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+  document.getElementById("header__input-menu").onclick = desplegar_index;
+};
+
+function desplegar_index() {
+  if (
+    document.getElementById("navegar").classList.contains("show")
+  ) {
+    document.getElementById("navegar").classList.remove("show");
+  } else {
+    document.getElementById("navegar").classList.add("show");
+  }
+} 
